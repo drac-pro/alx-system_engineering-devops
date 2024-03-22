4ae2f409d7f0f03446a1e45f93af17725748cbbd
Darius Tohtin <dariustohtin@gmail.com>
2024-03-22 07:58:45 -0700

    Edited

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,13 @@
 #Install flask
 
+exec { 'install pip3':
+  command => 'apt-get install -y python3-pip',
+  unless  => 'which pip3',
+  path    => ['/bin', '/usr/bin'],
+}
+
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Exec['install pip3']
 }
