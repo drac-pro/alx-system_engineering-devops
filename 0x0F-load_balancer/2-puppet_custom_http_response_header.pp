0f80cfa2cf64b2b304e1d7ef93e806ca1f4d6ba5
Darius Tohtin <dariustohtin@gmail.com>
2024-04-01 06:50:23 -0700

    2. Add a custom HTTP header with Puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -8,14 +8,16 @@ file_line { 'redirect':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://github.com/drac-pro permanent;'
+  line   => 'rewrite ^/redirect_me https://github.com/drac-pro permanent;',
+  require => Package['nginx']
 }
 
 file_line { 'custom_header':
   ensure => 'present',
   path   => '/etc/nginx/nginx.conf',
   after  => 'http {',
-  line   => 'add_header X-Served-By $::hostname;'
+  line   => 'add_header X-Served-By $hostname;',
+  require => Package['nginx']
 }
 
 file { '/var/www/html/index.html':
