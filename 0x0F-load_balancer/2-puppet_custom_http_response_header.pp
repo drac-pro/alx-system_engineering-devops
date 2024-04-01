dfcb6f7edebba54ef0039bb851a205a0667b4a3e
Darius Tohtin <dariustohtin@gmail.com>
2024-04-01 12:00:26 -0700

    2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -10,24 +10,13 @@ package { 'nginx':
   ensure => installed
 }
 
-file_line { 'redirect':
-  ensure  => 'present',
-  path    => '/etc/nginx/sites-available/default',
-  after   => 'listen 80 default_server;',
-  line    => 'rewrite ^/redirect_me https://github.com/drac-pro permanent;',
-  require => Package['nginx']
-}
-
 file_line { 'custom_header':
   ensure  => 'present',
   path    => '/etc/nginx/nginx.conf',
   after   => 'http {',
   line    => "add_header X-Served-By \"${hostname}\";",
-  require => Package['nginx']
-}
-
-file { '/var/www/html/index.html':
-  content => 'Hello World!'
+  require => Package['nginx'],
+  before  => Service['nginx']
 }
 
 service { 'nginx':
