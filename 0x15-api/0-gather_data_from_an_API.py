c31c8e06538988a3b3f34b67191c854d461f71fc
Darius Tohtin <dariustohtin@gmail.com>
2024-04-22 14:07:29 -0700

    0. Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""Write a Python script that, using this REST API, for a given employee ID,
-returns information about his/her TODO list progress."""
+"""Returns information about his/her TODO list progress."""
 import requests
 from sys import argv
 
