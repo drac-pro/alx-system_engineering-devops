878fe2bcd29ea1442ca63001db3e0682a7d97557
Darius Tohtin <dariustohtin@gmail.com>
2024-05-07 13:27:31 -0700

    edit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,19 +5,19 @@ import requests
 
 def number_of_subscribers(subreddit):
     """returns the number of subscribers for given subreddit
-
     args:
         subreddit: name of the subreddit
     Returns:
         number of subscribers
     """
     try:
-        headers = {'User-Agent': 'linux:apidriven.app:v1.2.3'}
+        headers = {'User-Agent': 'Python:SubredditSubscriberCounter:v1.2.3'}
         url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
         response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code == 200:
             return response.json()['data']['subscribers']
         else:
+            print(response.status_code)
             return 0
     except Exception:
         return 0
