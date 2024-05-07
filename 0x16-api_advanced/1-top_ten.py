2dce43478809dc5341ce477a94d57bd859a7771c
Darius Tohtin <dariustohtin@gmail.com>
2024-05-07 14:21:43 -0700

    1. Top Ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""defines a function for getting number of subscribers in a subreddit"""
+import requests
+
+
+def top_ten(subreddit):
+    """returns the number of subscribers for given subreddit
+    args:
+        subreddit: name of the subreddit
+    Returns:
+        number of subscribers
+    """
+    try:
+        headers = {'User-Agent':
+                   'Python:SubredditHotPosts:v1.2.3 (by /u/drac_prog)'}
+        url = "https://www.reddit.com/r/{}/hot.json?limit=9".format(subreddit)
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code != 200:
+            print(None)
+            return
+        [print(c['data']['title']) for c in
+         response.json()['data']['children']]
+    except Exception:
+        print(None)
