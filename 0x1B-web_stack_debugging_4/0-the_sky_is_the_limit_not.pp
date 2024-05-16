d56cf49be7c60e8dfc565c57b975a02d601bbedd
Darius Tohtin <dariustohtin@gmail.com>
2024-05-16 14:56:47 -0700

    0. Sky is the limit, let's bring that limit higher

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,11 @@
+# Fixes server to handle more request
+
+exec {'replace':
+  provider => shell,
+  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+  before   => Exec['restart'],
+}
+exec {'restart':
+  provider => shell,
+  command  => 'sudo service nginx restart',
+}
---
+++ 0x1B-web_stack_debugging_4/README.md
@@ -0,0 +1,5 @@
+# 0x1B. Web stack debugging #4
+
+![LOL](https://s3.amazonaws.com/intranet-projects-files/holbertonschool-sysadmin_devops/313/frdkCrb.jpg)
+
+Debuggin 4
